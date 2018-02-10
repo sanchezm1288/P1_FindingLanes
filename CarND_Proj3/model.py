1b5d5f1f85086e7a9cc40190fc4796b8b093c87c
Manuel Alejandro Sanchez <sanchezm1288@gmail.com>
2018-02-10 18:42:27 -0500

    Add files via upload

--- CarND_Proj3/model.py
+++ CarND_Proj3/model.py
@@ -1,89 +1,110 @@
-from keras.models import Model
-from keras.layers import Input, Convolution2D, Flatten, Dense, Dropout, ELU, Lambda
-from keras.callbacks import ModelCheckpoint, CSVLogger
-import keras.backend as K
-from config import *
-from load_data import generate_data_batch, split_train_val
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
+from utils import INPUT_SHAPE, batch_generator
+import argparse
+import os
+
+np.random.seed(0)
 
 
-def get_nvidia_model(summary=True):
+def load_data(args):
     """
-    Get the keras Model corresponding to the NVIDIA architecture described in:
-    Bojarski, Mariusz, et al. "End to end learning for self-driving cars."
-
-    The paper describes the network architecture but doesn't go into details for some aspects.
-    Input normalization, as well as ELU activations are just my personal implementation choice.
-
-    :param summary: show model summary
-    :return: keras Model of NVIDIA architecture
+    Load training data and split it into training and validation set
     """
-    init = 'glorot_uniform'
+    data_df = pd.read_csv(os.path.join(args.data_dir, 'driving_log.csv'))
 
-    if K.backend() == 'theano':
-        input_frame = Input(shape=(CONFIG['input_channels'], NVIDIA_H, NVIDIA_W))
-    else:
-        input_frame = Input(shape=(NVIDIA_H, NVIDIA_W, CONFIG['input_channels']))
+    X = data_df[['center', 'left', 'right']].values
+    y = data_df['steering'].values
 
-    # standardize input
-    x = Lambda(lambda z: z / 127.5 - 1.)(input_frame)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0)
 
-    x = Convolution2D(24, 5, 5, border_mode='valid', subsample=(2, 2), init=init)(x)
-    x = ELU()(x)
-    x = Dropout(0.2)(x)
-    x = Convolution2D(36, 5, 5, border_mode='valid', subsample=(2, 2), init=init)(x)
-    x = ELU()(x)
-    x = Dropout(0.2)(x)
-    x = Convolution2D(48, 5, 5, border_mode='valid', subsample=(2, 2), init=init)(x)
-    x = ELU()(x)
-    x = Dropout(0.2)(x)
-    x = Convolution2D(64, 3, 3, border_mode='valid', init=init)(x)
-    x = ELU()(x)
-    x = Dropout(0.2)(x)
-    x = Convolution2D(64, 3, 3, border_mode='valid', init=init)(x)
-    x = ELU()(x)
-    x = Dropout(0.2)(x)
+    return X_train, X_valid, y_train, y_valid
 
-    x = Flatten()(x)
 
-    x = Dense(100, init=init)(x)
-    x = ELU()(x)
-    x = Dropout(0.5)(x)
-    x = Dense(50, init=init)(x)
-    x = ELU()(x)
-    x = Dropout(0.5)(x)
-    x = Dense(10, init=init)(x)
-    x = ELU()(x)
-    out = Dense(1, init=init)(x)
-
-    model = Model(input=input_frame, output=out)
-
-    if summary:
-        model.summary()
+def build_model(args):
+    """
+    Modified NVIDIA model
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
+    model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(64, 3, 3, activation='elu'))
+    model.add(Conv2D(64, 3, 3, activation='elu'))
+    model.add(Dropout(args.keep_prob))
+    model.add(Flatten())
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1))
+    model.summary()
 
     return model
 
 
+def train_model(model, args, X_train, X_valid, y_train, y_valid):
+    """
+    Train the model
+    """
+    checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
+                                 monitor='val_loss',
+                                 verbose=0,
+                                 save_best_only=args.save_best_only,
+                                 mode='auto')
+
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=args.learning_rate))
+
+    model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),
+                        args.samples_per_epoch,
+                        args.nb_epoch,
+                        max_q_size=1,
+                        validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),
+                        nb_val_samples=len(X_valid),
+                        callbacks=[checkpoint],
+                        verbose=1)
+
+
+def s2b(s):
+    """
+    Converts a string to boolean value
+    """
+    s = s.lower()
+    return s == 'true' or s == 'yes' or s == 'y' or s == '1'
+
+
+def main():
+    """
+    Load train/validation data set and train the model
+    """
+    parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
+    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='data')
+    parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
+    parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
+    parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=10)
+    parser.add_argument('-s', help='samples per epoch',     dest='samples_per_epoch', type=int,   default=20000)
+    parser.add_argument('-b', help='batch size',            dest='batch_size',        type=int,   default=40)
+    parser.add_argument('-o', help='save best models only', dest='save_best_only',    type=s2b,   default='true')
+    parser.add_argument('-l', help='learning rate',         dest='learning_rate',     type=float, default=1.0e-4)
+    args = parser.parse_args()
+
+    print('-' * 30)
+    print('Parameters')
+    print('-' * 30)
+    for key, value in vars(args).items():
+        print('{:<20} := {}'.format(key, value))
+    print('-' * 30)
+
+    data = load_data(args)
+    model = build_model(args)
+    train_model(model, args, *data)
+
+
 if __name__ == '__main__':
+    main()
 
-    # split udacity csv data into training and validation
-    train_data, val_data = split_train_val(csv_driving_data='data/driving_log.csv')
-
-    # get network model and compile it (default Adam opt)
-    nvidia_net = get_nvidia_model(summary=True)
-    nvidia_net.compile(optimizer='adam', loss='mse')
-
-    # json dump of model architecture
-    with open('logs/model.json', 'w') as f:
-        f.write(nvidia_net.to_json())
-
-    # define callbacks to save history and weights
-    checkpointer = ModelCheckpoint('checkpoints/weights.{epoch:02d}-{val_loss:.3f}.hdf5')
-    logger = CSVLogger(filename='logs/history.csv')
-
-    # start the training
-    nvidia_net.fit_generator(generator=generate_data_batch(train_data, augment_data=True, bias=CONFIG['bias']),
-                         samples_per_epoch=300*CONFIG['batchsize'],
-                         nb_epoch=50,
-                         validation_data=generate_data_batch(val_data, augment_data=False, bias=1.0),
-                         nb_val_samples=100*CONFIG['batchsize'],
-                         callbacks=[checkpointer, logger])
